34a4007baa507a9899595ecba650f0cdf36c54d0
Phillip Whelan <phil@calyptia.com>
2023-08-21 21:02:11 -0400

    custom_calyptia: set fleet_id for out_calyptia.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- plugins/custom_calyptia/calyptia.c
+++ plugins/custom_calyptia/calyptia.c
@@ -309,6 +309,10 @@ static struct flb_output_instance *setup_cloud_output(struct flb_config *config,
         flb_output_set_property(cloud, "tls.verify", "false");
     }
 
+    if (ctx->fleet_id) {
+        flb_output_set_property(cloud, "fleet_id", ctx->fleet_id);
+    }
+
 #ifdef FLB_HAVE_CHUNK_TRACE
     flb_output_set_property(cloud, "pipeline_id", ctx->pipeline_id);
 #endif /* FLB_HAVE_CHUNK_TRACE */
